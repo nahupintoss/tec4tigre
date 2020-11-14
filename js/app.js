eda03a2ea7d319fa1dbcb3e4a2c6b6705ace7fce
nahupintoss <74433610+nahupintoss@users.noreply.github.com>
2020-11-14 13:07:16 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,112 +1,77 @@
-particlesJS({
-    {
-        "particles": {
-          "number": {
-            "value": 100,
-            "density": {
-              "enable": true,
-              "value_area": 800
-            }
-          },
-          "color": {
-            "value": "#ffffff"
-          },
-          "shape": {
-            "type": "circle",
-            "stroke": {
-              "width": 0,
-              "color": "#000000"
-            },
-            "polygon": {
-              "nb_sides": 5
-            },
-            "image": {
-              "src": "img/github.svg",
-              "width": 100,
-              "height": 100
-            }
-          },
-          "opacity": {
-            "value": 0.5,
-            "random": false,
-            "anim": {
-              "enable": false,
-              "speed": 1,
-              "opacity_min": 0.1,
-              "sync": false
-            }
-          },
-          "size": {
-            "value": 3,
-            "random": true,
-            "anim": {
-              "enable": false,
-              "speed": 40,
-              "size_min": 0.1,
-              "sync": false
-            }
-          },
-          "line_linked": {
-            "enable": true,
-            "distance": 173.61248115909999,
-            "color": "#ffffff",
-            "opacity": 0.4,
-            "width": 1
-          },
-          "move": {
-            "enable": true,
-            "speed": 6,
-            "direction": "none",
-            "random": false,
-            "straight": false,
-            "out_mode": "out",
-            "bounce": false,
-            "attract": {
-              "enable": false,
-              "rotateX": 600,
-              "rotateY": 1200
-            }
-          }
-        },
-        "interactivity": {
-          "detect_on": "canvas",
-          "events": {
-            "onhover": {
-              "enable": true,
-              "mode": "repulse"
-            },
-            "onclick": {
-              "enable": true,
-              "mode": "push"
-            },
-            "resize": true
-          },
-          "modes": {
-            "grab": {
-              "distance": 400,
-              "line_linked": {
-                "opacity": 1
-              }
-            },
-            "bubble": {
-              "distance": 400,
-              "size": 40,
-              "duration": 2,
-              "opacity": 8,
-              "speed": 3
-            },
-            "repulse": {
-              "distance": 200,
-              "duration": 0.4
-            },
-            "push": {
-              "particles_nb": 4
-            },
-            "remove": {
-              "particles_nb": 2
-            }
-          }
-        },
-        "retina_detect": true
-      }
-})
+/* Navegation Menu */
+const menu = document.querySelector('.menu');
+const linkContainer = document.querySelector('.list-container');
+const main = document.querySelector('#main');
+const sticky = menu.offsetTop;
+const btnMenu = document.querySelector('.btn-menu');
+let on_off = true;
+
+
+window.onscroll = () => {
+    
+    // Sticky Menu
+    if (window.pageYOffset >= sticky) {
+       menu.classList.add('sticky');
+       main.style.marginTop = '170px';
+    }else{
+        menu.classList.remove('sticky');
+        main.style.marginTop = '100px'; 
+    }
+
+    // Go Top Button
+    if (window.pageYOffset >= 500) {
+        document.querySelector('.go-top').style.right = "0%";
+    }else{
+        document.querySelector('.go-top').style.right = "-100%";
+    }
+
+    document.querySelector('.go-top').addEventListener('click', () => {
+        document.body.scrollTop = '0';
+        document.documentElement.scrollTop = '0';
+    });
+
+}
+
+// Responsive Menu
+btnMenu.addEventListener('click', () => {
+    
+    if (on_off) {
+       linkContainer.style.left = "0";
+       
+       on_off = false;
+    }else{
+        on_off = false;
+        linkContainer.style.left = "-100%";
+        on_off = true;
+    }
+
+});
+
+
+// Gallery
+const images = document.querySelectorAll('.img-card img');
+const modal = document.querySelector('.modal-gallery');
+const imgModal = document.querySelector('#imgModal');
+const closeModal = document.querySelector('.close-modal');
+const captionImg = document.querySelector('.captionModal');
+const modalGalleryList = document.querySelectorAll('.gallery-list');
+
+images.forEach((images) => {
+   images.addEventListener('click', () => {
+       modal.style.display = "block";
+       imgModal.src = images.src;
+       captionImg.innerHTML = images.alt;
+   });
+});
+
+modalGalleryList.forEach(img => img.addEventListener('click', lightbox));
+
+function lightbox (event) {
+  imgModal.src = event.target.src;
+
+  captionImg.innerHTML = event.target.alt;
+}
+
+closeModal.addEventListener('click', () => {
+    modal.style.display = "none";
+});
